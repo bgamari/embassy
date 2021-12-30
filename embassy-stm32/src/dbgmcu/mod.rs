6827b7e64347b28932e67a525566c0baa918f3e3
Ben Gamari <ben@smart-cactus.org>
2021-12-29 19:22:17 -0500

    embassy-stm32/g0: Ensure that DBGMCU is ungated when necessary

--- embassy-stm32/src/dbgmcu/mod.rs
+++ embassy-stm32/src/dbgmcu/mod.rs
@@ -2,6 +2,9 @@ pub struct Dbgmcu {}
 
 impl Dbgmcu {
     pub unsafe fn enable_all() {
+        #[cfg(rcc_g0)]
+        crate::pac::RCC.apbenr1().modify(|w| w.set_dbgen(true));
+
         crate::pac::DBGMCU.cr().modify(|cr| {
             crate::pac::dbgmcu! {
                 (cr, $fn_name:ident) => {
